f994361299853c28e8e284cfeed0979de25fc38c
Nick DeNardis <nick.denardis@gmail.com>
2020-03-04 19:35:53 -0500

    Config added for GitHub build

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: "IMT 549 - Intro to Web Development",
+    description: "Intro to Web Development",
+    base: "/coursework/",
+    head: [["link", { rel: "shortcut icon", href: "/favicon.ico" }]],
+    ga: "UA-116416182-3",
+    themeConfig: {
+      sidebar: [
+          "/", 
+      ]
+    }
+  };
+  
